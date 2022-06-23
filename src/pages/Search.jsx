24d65f78406df71f7d68ab78765b6d7c5ec6a63c
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-06-23 19:20:56 -0400

    put random dog in card, page heading style and fix form width

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,38 +1,38 @@
 // show random dog from DB
-//dislplay all dog info 
+//dislplay all dog info
 // user has the option to like or diskile a dog
 // liked dogs get saved to the dog model and DB and removed from array
 // disliked dogs get removed from the array of dogs from DB
 // if both users like each other their profiles get saved in matches
-import React, { useEffect, useState } from 'react';
-import { authAxios } from '../customAxios/authAxios';
+import React, { useEffect, useState } from "react";
+import { authAxios } from "../customAxios/authAxios";
 
 function Search() {
   const [dog, setDog] = useState({});
 
-  useEffect(() => { 
-    authAxios.get('http://localhost:5005/api/matches/find')
-      .then(response => {
-        setDog(response.data)
-      })
+  useEffect(() => {
+    authAxios.get("http://localhost:5005/api/matches/find").then((response) => {
+      setDog(response.data);
+    });
   }, []);
 
   return (
     <>
-    <div>Search</div>
-    <ul>
-        <img src={dog.imageUrl} alt="dog" width={200}/>
-        <li>{dog.name}</li>
-        <li>{dog.age}</li>
-        <li>{dog.gender}</li>
-        <li>{dog.size}</li>
-        <li>{dog.breed}</li>
-        <li>{dog.about}</li>
-
-      
-    </ul>
+      <h2>Search</h2>
+      <div class="card content-small">
+        <img src={dog.imageUrl} alt="dog" width={200} />
+        <h3>{dog.name}</h3>
+        <div>
+          {dog.age} year(s) old
+          <br />
+          {dog.gender} <br />
+          {dog.size} <br />
+          {dog.breed} <br />
+          {dog.about} <br />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Search
+export default Search;
